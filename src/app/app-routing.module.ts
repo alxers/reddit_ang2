b9375cacd503578cc06f29af11492fce3c1f80a3
Alexey Ershov <alex4rom@gmail.com>
2017-05-05 17:30:58 +0300

    Add outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule }              from '@angular/core';
-import { RouterModule, Routes }  from '@angular/router';
-import { LoginComponent }   from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 // import { PageNotFoundComponent } from './not-found.component';
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '',   redirectTo: '/', pathMatch: 'full', canActivate: [AuthGuard] },
   // { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
